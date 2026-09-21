0d92343d295345a444aee828cbaf240cc80b98f7
AbhishekRai11 <92274417+AbhishekRai11@users.noreply.github.com>
2026-09-21 21:03:35 +0530

    Add src/main/java/com/abhishek/hotelbooking/repository/HotelRepository.java

---
+++ src/main/java/com/abhishek/hotelbooking/repository/HotelRepository.java
@@ -0,0 +1,1 @@
+package com.abhishek.hotelbooking.repository; import com.abhishek.hotelbooking.entity.Hotel; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface HotelRepository extends JpaRepository<Hotel,Long>{ List<Hotel> findByCityIgnoreCase(String city); }
