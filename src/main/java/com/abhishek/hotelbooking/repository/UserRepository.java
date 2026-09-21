0e2447e745be87bdae202d097669982f20173c19
AbhishekRai11 <92274417+AbhishekRai11@users.noreply.github.com>
2026-09-21 21:03:32 +0530

    Add src/main/java/com/abhishek/hotelbooking/repository/UserRepository.java

---
+++ src/main/java/com/abhishek/hotelbooking/repository/UserRepository.java
@@ -0,0 +1,1 @@
+package com.abhishek.hotelbooking.repository; import com.abhishek.hotelbooking.entity.User; import java.util.*; import org.springframework.data.jpa.repository.JpaRepository; public interface UserRepository extends JpaRepository<User,Long>{ Optional<User> findByEmail(String email); boolean existsByEmail(String email); }
